60d732cd18aae9efcbbe8659e2e084751c21f138
unknown <sobakoved@inbox.lv>
2023-04-18 23:06:14 +0100

    Adds background image

---
+++ src/Components/Jumbotron/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+
+
+
+function Jumbotron() {
+  return (
+    
+    <div 
+    
+    >
+       
+      <h1><span>Your</span><span> YouTube</span></h1>
+      <h1><span>Your</span><span> Way</span></h1>
+    </div>
+  );
+}
+
+export default Jumbotron;
